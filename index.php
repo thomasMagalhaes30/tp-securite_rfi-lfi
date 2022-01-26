98bd194df1ea933d76f42f5c9d1eecd5dd4e97fb
Thomas MAGALHAES <thmagalhae@IUTCLINFA2319.iut.local>
2022-01-26 17:51:47 +0100

    query param in index

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <div>
         <h3 class="lfi-color">Les exos FLI</h3>
         <ul>
-            <li><a href="exo_lfi/">Exo 1 LFI</a></li>
-            <li><a href="exo_lfi_2/">Exo 2 LFI</a></li>
+            <li><a href="exo_lfi/?file=normal.html">Exo 1 LFI</a></li>
+            <li><a href="exo_lfi_2/?file=normal">Exo 2 LFI</a></li>
         </ul>
         <h3 class="rfi-color">Les exos RFI</h3>
         <ul>
