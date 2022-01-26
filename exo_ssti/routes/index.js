fa4592dd8a6da25ae74eaedfc467df18942c5bd0
Guillaume Valentin <guillaume.valentin182@gmail.com>
2022-01-26 19:39:20 +0100

    wip

--- exo_ssti/routes/index.js
+++ exo_ssti/routes/index.js
@@ -9,12 +9,20 @@ router.get('/', function(req, res, next) {
 
 // router.get('/lol', function(req, res, next) {
 //   let recievedCom = req.query.com
+//   console.log('Recieved: '.concat(recievedCom))
 //   res.render('index', { title: recievedCom });
 // });
 
 router.get('/:com', function(req, res, next) {
   let recievedCom = req.params.com
+  console.log('Recieved: '.concat(recievedCom))
   res.render('index', { title: recievedCom });
 });
+
+// router.get('/', function(req, res, next) {
+//   let recievedCom = req.body
+//   console.log('Recieved: '.concat(recievedCom))
+//   res.render('index', { title: recievedCom.nom });
+// });
 
 module.exports = router;
